851636e8b4e3d24f715913a7e0f4bbc3282e64dd
Isuru <isuruumanga123@gmail.com>
2020-08-24 18:39:13 +0530

    Sample login page

---
+++ app/src/main/java/video/sample/MainActivity.java
@@ -0,0 +1,14 @@
+package video.sample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
